5374b9a4e697a0f23f9a3aa94cab7bbe69faf1b2
lrobinsonco <lrobinsonco@gmail.com>
2017-07-06 14:33:47 -0600

    added db

--- conserve/backend/server.js
+++ conserve/backend/server.js
@@ -2,5 +2,16 @@ import express from 'express';
 import mongodb from 'mongodb'
 
 const app = express();
+const dbUrl = 'mongodb://localhost/crudwithredux';
 
-app.listen(8080, () => console.log('Server on localhost:8080'))
+mongodb.MongoClient.connect(dbUrl, function(err, db){
+
+  app.get('/api/orgs', (req,res) => {
+    db.collection('orgs').find({}).toArray((err, orgs) => {
+      res.json({ orgs });
+    });
+  });
+
+  app.listen(8080, () => console.log('Server on localhost:8080'))
+
+})
